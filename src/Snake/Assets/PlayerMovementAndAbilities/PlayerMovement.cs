fe2bc43b20dce4eb813b19a00b2f2b01a7a806e6
OlegMorev <proks200413@gmail.com>
2023-10-26 01:12:07 +0300

    Everything done

--- src/Snake/Assets/PlayerMovementAndAbilities/PlayerMovement.cs
+++ src/Snake/Assets/PlayerMovementAndAbilities/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,26 +12,66 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float _speedWhenBoosted;
     [SerializeField] int _shieldTime;
     [SerializeField] GameObject _stonePrefab;
-    float playerSpeed;
+    float realPlayerSpeed;
+    float theoreticalPlayerSpeed;
     Direction currentDirection;
     Direction nextDirection;
     Transform playerTransform;
     Vector2 playerPosition;
     Ability currentAbility;
+    bool isPlayerInCellCenter;
+    Vector2 playerCellCoordinates;
     void Start()
     {
         currentAbility = Ability.no;
-        playerSpeed = _basePlayerSpeed;
+        theoreticalPlayerSpeed = _basePlayerSpeed;
+        realPlayerSpeed = 0;
         playerTransform = GetComponent<Transform>();
     }
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<AbilityCheck>(out AbilityCheck abilityCheck))
+        {
+            if (abilityCheck._thisItem == Ability.speedBoost)
+            {
+                SetAbility(Ability.speedBoost);
+            }
+            if (abilityCheck._thisItem == Ability.stone)
+            {
+                SetAbility(Ability.stone);
+            }
+            if (abilityCheck._thisItem == Ability.shield)
+            {
+                SetAbility(Ability.shield);
+            }
+            if (abilityCheck._thisItem == Ability.plusPoints)
+            {
+                // ƒобавить к текущему количеству очков столько, сколько надо за предмет с доп очками (ещЄ, вроде, зме€ вырастает)
+            }
+            Destroy(collision.gameObject);
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.TryGetComponent<Barrier>(out Barrier barrier))
+        {
+            playerPosition.x = (float)Math.Round(playerPosition.x,1);
+            playerPosition.y = (float)Math.Round(playerPosition.y,1);
+            realPlayerSpeed = 0;
+            StartCoroutine(BarrierDisappear(barrier.gameObject));
+        }
+    }
     void Update()
     {
+        isPlayerInCellCenter = ((Mathf.Round((playerTransform.position.x - 0.5f) * 10f) / 10f) % 1.00 == 0 && (Mathf.Round((playerTransform.position.y - 0.5f) * 10f) / 10f) % 1.00 == 0);
         ChooseNextDirection();
         ChangeDirection();
         Movement();
         if(Input.GetKeyDown(KeyCode.Space)) UseAbility(currentAbility);
-        Debug.Log(currentAbility);
+        if (realPlayerSpeed != 0)
+        {
+            realPlayerSpeed = theoreticalPlayerSpeed;
+        }
     }
     void ChooseNextDirection()
     {
@@ -53,12 +94,13 @@ public class PlayerMovement : MonoBehaviour
     }
     void ChangeDirection()
     {
-        if(nextDirection != currentDirection)
+        if (nextDirection != currentDirection)
         {
-            if((Mathf.Round(playerTransform.position.x * 10f) / 10f) % 1.00 == 0 && (Mathf.Round(playerTransform.position.y * 10f) / 10f) % 1.00 == 0)
+            if(isPlayerInCellCenter)
             {
                 currentDirection = nextDirection;
             }
+            realPlayerSpeed = theoreticalPlayerSpeed;
         }
     }
     void Movement()
@@ -66,23 +108,23 @@ public class PlayerMovement : MonoBehaviour
         playerPosition = playerTransform.position;
         if(currentDirection == Direction.up)
         {
-            playerPosition.y += playerSpeed * Time.deltaTime;
-            playerPosition.x = Mathf.Round(playerPosition.x);
+            playerPosition.y += realPlayerSpeed * Time.deltaTime;
+            playerPosition.x = Mathf.Round(playerPosition.x * 10) / 10;
         }
         if (currentDirection == Direction.down)
         {
-            playerPosition.y -= playerSpeed * Time.deltaTime;
-            playerPosition.x = Mathf.Round(playerPosition.x);
+            playerPosition.y -= realPlayerSpeed * Time.deltaTime;
+            playerPosition.x = Mathf.Round(playerPosition.x * 10) / 10;
         }
         if (currentDirection == Direction.left)
         {
-            playerPosition.x -= playerSpeed * Time.deltaTime;
-            playerPosition.y = Mathf.Round(playerPosition.y);
+            playerPosition.x -= realPlayerSpeed * Time.deltaTime;
+            playerPosition.y = Mathf.Round(playerPosition.y * 10) / 10;
         }
         if (currentDirection == Direction.right)
         {
-            playerPosition.x += playerSpeed * Time.deltaTime;
-            playerPosition.y = Mathf.Round(playerPosition.y);
+            playerPosition.x += realPlayerSpeed * Time.deltaTime;
+            playerPosition.y = Mathf.Round(playerPosition.y * 10) / 10;
         }
         playerTransform.position = playerPosition;
     }
@@ -117,21 +159,40 @@ public class PlayerMovement : MonoBehaviour
     }
     IEnumerator SpeedUpAbilityTimer(int time)
     {
-        playerSpeed = _speedWhenBoosted;
+        theoreticalPlayerSpeed = _speedWhenBoosted;
         yield return new WaitForSeconds(time);
-        playerSpeed = _basePlayerSpeed;
+        theoreticalPlayerSpeed = _basePlayerSpeed;
         yield break;
     }
     IEnumerator ShieldAbilityTimer(int time)
     {
-        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        gameObject.layer = 6;
         yield return new WaitForSeconds(time);
-        gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        gameObject.layer = 0;
         yield break;
+    }
+    IEnumerator BarrierDisappear(GameObject go)
+    {
+        while(go != null)
+        {
+            yield return null;
+        }
+        realPlayerSpeed = theoreticalPlayerSpeed;
+        yield break;
+    }
+    public Vector2 SendPlayerCoordinates()
+    {
+        if (isPlayerInCellCenter)
+        {
+            playerCellCoordinates.x = Convert.ToInt32(playerTransform.position.x - 0.5f);
+            playerCellCoordinates.y = Convert.ToInt32(playerTransform.position.y - 0.5f);
+        }
+        return playerCellCoordinates;
     }
 }
 public enum Direction
 {
+    no,
     left,
     right,
     up,
