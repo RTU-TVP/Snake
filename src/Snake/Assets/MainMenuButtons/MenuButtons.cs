45c5c81aaf856fd078e5d1cdd13ec822c9576f4b
krl76 <k.kornilov2005@yandex.ru>
2023-10-29 02:48:04 +0300

    Customize

--- src/Snake/Assets/MainMenuButtons/MenuButtons.cs
+++ src/Snake/Assets/MainMenuButtons/MenuButtons.cs
@@ -7,18 +7,25 @@ using UnityEngine.UI;
 public class MenuButtons : MonoBehaviour
 {
     [SerializeField] Button _playButton;
+    [SerializeField] Button _chooseButton;
     [SerializeField] Button _exitButton;
     [SerializeField] GameObject _modeChooseCanvas;
+    [SerializeField] GameObject _modeChooseWearCanvas;
     Action play;
+    Action choose;
     Action exit;
     private void Awake()
     {
         _modeChooseCanvas.SetActive(false);
+        _modeChooseWearCanvas.SetActive(false);
         play = PlayButton;
+        choose = ChooseButton;
         exit = ExitButton;
         _playButton.onClick.RemoveAllListeners();
+        _chooseButton.onClick.RemoveAllListeners();
         _exitButton.onClick.RemoveAllListeners();
         _playButton.onClick.AddListener(play.Invoke);
+        _chooseButton.onClick.AddListener(choose.Invoke);
         _exitButton.onClick.AddListener(exit.Invoke);
     }
     public void ExitButton()
@@ -30,6 +37,16 @@ public class MenuButtons : MonoBehaviour
     {
         _modeChooseCanvas.SetActive(true);
         _playButton.interactable = false;
+        _chooseButton.interactable = false;
+        _exitButton.interactable = false;
+
+    }
+
+    public void ChooseButton()
+    {
+        _modeChooseWearCanvas.SetActive(true);
+        _playButton.interactable = false;
+        _chooseButton.interactable = false;
         _exitButton.interactable = false;
 
     }
