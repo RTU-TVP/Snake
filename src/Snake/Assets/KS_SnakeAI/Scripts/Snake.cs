c106b4a46f55d83a08dd5a1c7a6c1727538a8aef
KotovSergeyV <75029707+KotovSergeyV@users.noreply.github.com>
2023-10-29 02:50:12 +0300

    Turning

--- src/Snake/Assets/KS_SnakeAI/Scripts/Snake.cs
+++ src/Snake/Assets/KS_SnakeAI/Scripts/Snake.cs
@@ -23,8 +23,12 @@ public class Snake : MonoBehaviour
     [field:SerializeField] public List<BoxCollider2D> ColliderList { get; private set; } =  new List<BoxCollider2D>();
     
     [SerializeField] private GameObject _snakePart;    
-    [SerializeField] private GameObject _snakeTail;    
-
+    [SerializeField] private GameObject _snakeTail;
+    
+    [SerializeField] private GameObject _turnSpriteRD;  
+    [SerializeField] private GameObject _turnSpriteLD;  
+    [SerializeField] private GameObject _turnSpriteRU;  
+    [SerializeField] private GameObject _turnSpriteLU;  
 
     [SerializeField] private int _partsOfSnake=1;
     private float _partsAddTimer;
@@ -33,7 +37,7 @@ public class Snake : MonoBehaviour
     [SerializeField] private float _stanTimerCap=0;
     [SerializeField] private bool _classicMode=true;
     public PathFinder PathFinder { get; private set; }
-
+    private Vector2 _direction;
     public void CurrentPosSetter(Vector2 pos)
     {
         _currentNode.XYSet((int)pos.x, (int)pos.y);
@@ -49,6 +53,7 @@ public class Snake : MonoBehaviour
         _headCol = transform.GetChild(0).GetComponent<BoxCollider2D>();
         ColliderList.Add(_headCol);
         CreationParts(1);
+        _direction = new Vector2(-1, 0);
     }
 
     private void AddToTail()
@@ -86,8 +91,8 @@ public class Snake : MonoBehaviour
         SnakePart part = snakePart.GetComponent<SnakePart>();
         part.Setter(PathFinder.Grid,
             PathFinder.Grid.GetValue(_currentNode.X + xAdd, _currentNode.Y+yAdd), _currentNode, _cellSize, _position, _speed);
-        part._currentNode.isWalkable = true;
-        part._targetNode.isWalkable = false;
+        part.CurrentNode.isWalkable = true;
+        part.TargetNode.isWalkable = false;
         part.transform.GetComponentInChildren<ImageRotationP>().Snake = part.GetComponent<SnakePart>();
         _snakeParts.Add(part);
         ColliderList.Add(snakePart.GetComponentInChildren<BoxCollider2D>());
@@ -98,11 +103,11 @@ public class Snake : MonoBehaviour
         snakePart.AddComponent<SnakePart>();
         SnakePart part = snakePart.GetComponent<SnakePart>();
         var x = (_snakeParts.Count-1);
-        var node = _snakeParts[x]._currentNode;
+        var node = _snakeParts[x].CurrentNode;
         part.Setter(PathFinder.Grid,
             PathFinder.Grid.GetValue(node.X + xAdd, node.Y+yAdd), node, _cellSize, _position, _speed);
-        part._currentNode.isWalkable = true;
-        part._targetNode.isWalkable = false;
+        part.CurrentNode.isWalkable = true;
+        part.TargetNode.isWalkable = false;
         part.transform.GetComponentInChildren<ImageRotationP>().Snake = part.GetComponent<SnakePart>();
         _snakeParts[x].child = part;
         _snakeParts.Add(part);
@@ -137,34 +142,29 @@ public class Snake : MonoBehaviour
     {
         if (_stanTimer <= 0)
         {
-            if (_partsAddTimer >= _partAddTimerCap)
-            {
+            if (_partsAddTimer >= _partAddTimerCap) {
                 AddToTail();
                 _partsAddTimer = 0;
             }
-            else
-            {
-                _partsAddTimer += Time.deltaTime;
-            }
+            else _partsAddTimer += Time.deltaTime;
 
-            if (!_isMoving)
-            {
+            if (!_isMoving) {
                 PlayerPositioning();
-                if (!(_targetNode.X == _currentNode.X && _targetNode.Y == _currentNode.Y))
-                {
+                
+                if (!(_targetNode.X == _currentNode.X && _targetNode.Y == _currentNode.Y)) {
                     _nextNode = PathFinder.FindStep(_currentNode.X, _currentNode.Y, _targetNode.X, _targetNode.Y);
-                    if (_nextNode is null)
-                    {
+                    if (_nextNode is null) {
                         PathNode neighbour = FindNeighbour();
 
-                        if (neighbour is null)
-                        {
-                            Destroy(gameObject);
-                        }
-                        else
-                        {
-                            _nextNode = neighbour;
-                        }
+                        if (neighbour is null) Destroy(gameObject);
+                        else _nextNode = neighbour;
+                    }
+
+                    var x = GetDirection();
+                    if (x != _direction)
+                    {
+                        StartCoroutine(SpawnTurnSprite(_direction, x));
+                        _direction = x;
                     }
 
                     TargetPosition = new Vector3(_nextNode.X * _cellSize + _cellSize * 0.5f + _position.x,
@@ -173,20 +173,19 @@ public class Snake : MonoBehaviour
                     transform.GetComponentInChildren<ImageRotation>().UpdateImageRotation();
                 }
             }
-            else
-            {
+            else {
                 transform.position += (TargetPosition - transform.position).normalized * (_cellSize * _speed) *
                                       Time.deltaTime;
                 UpdatePartsMovement();
                 if (new Vector3(Mathf.Round(transform.position.x * 10) / 10,
-                        Mathf.Round(transform.position.y * 10) / 10, 0) == TargetPosition)
-                {
+                        Mathf.Round(transform.position.y * 10) / 10, 0) == TargetPosition) {
                     _currentNode = _nextNode;
                     _snakeParts[0].SetTarget(_currentNode);
                     _isMoving = false;
                 }
             }
         }
+        
         else _stanTimer -= Time.deltaTime;
     }
 
@@ -213,6 +212,39 @@ public class Snake : MonoBehaviour
         {
             snakePart.UpdateMovement();
         }
+    }
+
+
+    private IEnumerator SpawnTurnSprite(Vector2 rotateFrom, Vector2 rotateTo)
+    {
+        yield return new WaitForSeconds(0.1f);
+        GameObject turn = null;
+        turn = TurnImageRotation(rotateFrom, rotateTo, turn);
+        yield return new WaitForSeconds(_snakeParts.Count * 0.21f);
+        Destroy(turn);
+    }
+
+    private GameObject TurnImageRotation(Vector2 rotateFrom, Vector2 rotateTo, GameObject turn)
+    {
+        if ((rotateFrom == Vector2.right && rotateTo == Vector2.down) || (rotateFrom == Vector2.up && rotateTo == Vector2.left))
+            turn = Instantiate(_turnSpriteRD, new Vector3(_currentNode.X + 0.5f, _currentNode.Y + 0.5f,-2f),
+                Quaternion.identity);
+        
+        else if ((rotateFrom == Vector2.left && rotateTo == Vector2.down) || (rotateFrom == Vector2.up && rotateTo == Vector2.right))
+            turn = Instantiate(_turnSpriteLD, new Vector3(_currentNode.X + 0.5f, _currentNode.Y + 0.5f,-2f),
+                Quaternion.identity);
+        
+        else if ((rotateFrom == Vector2.right && rotateTo == Vector2.up) || (rotateFrom == Vector2.down && rotateTo == Vector2.left))
+            turn = Instantiate(_turnSpriteRU, new Vector3(_currentNode.X + 0.5f, _currentNode.Y + 0.5f,-2f),
+                Quaternion.identity);
+        
+        
+        else if ((rotateFrom == Vector2.left && rotateTo == Vector2.up) || (rotateFrom == Vector2.down && rotateTo == Vector2.right))
+            turn = Instantiate(_turnSpriteLU, new Vector3(_currentNode.X + 0.5f, _currentNode.Y + 0.5f, -2f),
+                Quaternion.identity);
+        
+        
+        return turn;
     }
 
     private void Print()
--- src/Snake/Assets/KS_SnakeAI/Scripts/SnakePart.cs
+++ src/Snake/Assets/KS_SnakeAI/Scripts/SnakePart.cs
@@ -9,58 +9,70 @@ public class SnakePart : MonoBehaviour
 {
     [SerializeField] private int _cellSize;
 
-    [field:SerializeField] public PathNode _currentNode { get; private set; }
-    [field:SerializeField] public PathNode _targetNode { get; private set; }
+    [field:SerializeField] public PathNode CurrentNode { get; private set; }
+    [field:SerializeField] public PathNode TargetNode { get; private set; }
 
     [SerializeField] private float _speed;
     [SerializeField] private Vector2 _position;
-    [SerializeField] private bool _isMoving;
     [SerializeField] public Vector3 TargetPosition { get; private set; }
     [SerializeField] public SnakePart child;
-    
+
+    private Color _childColorCopy;
+    private SpriteRenderer _childSpriteRenderer;
     private CustomGrid<PathNode> _grid;
     
     public void Setter(CustomGrid<PathNode> grid,  PathNode currentNode,PathNode targetNode, int cellSize, Vector2 pos, float speed)
     {
-        _currentNode = currentNode;
-        _targetNode = targetNode;
+        _childColorCopy = GetComponentInChildren<SpriteRenderer>().color;
+        _childSpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        CurrentNode = currentNode;
+        TargetNode = targetNode;
         _cellSize = cellSize;
         _position = pos;
         _speed = speed;
         _grid = grid;
-        TargetPosition = new Vector3(_targetNode.X * _cellSize + _cellSize * 0.5f + pos.x,
-            _targetNode.Y * _cellSize + _cellSize * 0.5f+pos.y, 0);
-        _grid.GetValue(_currentNode.X, _currentNode.Y).isWalkable = false;
-        transform.position = new Vector3(_currentNode.X * _cellSize + _cellSize * 0.5f+ pos.x, _currentNode.Y * _cellSize + _cellSize * 0.5f+ pos.y, 0);
+        TargetPosition = new Vector3(TargetNode.X * _cellSize + _cellSize * 0.5f + pos.x,
+            TargetNode.Y * _cellSize + _cellSize * 0.5f+pos.y, 0);
+        _grid.GetValue(CurrentNode.X, CurrentNode.Y).isWalkable = false;
+        transform.position = new Vector3(CurrentNode.X * _cellSize + _cellSize * 0.5f+ pos.x, CurrentNode.Y * _cellSize + _cellSize * 0.5f+ pos.y, 0);
         transform.localScale = new Vector3( _cellSize, _cellSize, 0f);
     }
     
 
     public void SetTarget(PathNode target)
     {
-        _currentNode.isWalkable = true;
+        CurrentNode.isWalkable = true;
         
-        _currentNode = _targetNode;        
+        CurrentNode = TargetNode;        
 
         if (child is not null)
         {
-            child.SetTarget(_currentNode);
-            _currentNode.isWalkable = false;
+            child.SetTarget(CurrentNode);
+            CurrentNode.isWalkable = false;
         }
         
-        _targetNode = target;
-        _targetNode.isWalkable = false;
-        TargetPosition = new Vector3(_targetNode.X * _cellSize + _cellSize * 0.5f+ _position.x,
-            _targetNode.Y * _cellSize + _cellSize * 0.5f+ _position.y, 0);
+        TargetNode = target;
+        TargetNode.isWalkable = false;
+        TargetPosition = new Vector3(TargetNode.X * _cellSize + _cellSize * 0.5f+ _position.x,
+            TargetNode.Y * _cellSize + _cellSize * 0.5f+ _position.y, 0);
         
         transform.GetComponentInChildren<ImageRotationP>().UpdateImageRotation();
     }
     public Vector2 GetDirection()
     {
-        return (_targetNode.Positioning() - _currentNode.Positioning()).normalized;
+        return (TargetNode.Positioning() - CurrentNode.Positioning()).normalized;
     }
     public void UpdateMovement()
     {
         transform.position += (TargetPosition-transform.position).normalized * (_cellSize * _speed) * Time.deltaTime;
+    }
+    
+    private IEnumerator Turning()
+    {
+        _childColorCopy.a = 0;
+        _childSpriteRenderer.color = _childColorCopy;
+        yield return new WaitForSeconds(0.08f);
+        _childColorCopy.a = 255;
+        _childSpriteRenderer.color = _childColorCopy;
     }
 }
