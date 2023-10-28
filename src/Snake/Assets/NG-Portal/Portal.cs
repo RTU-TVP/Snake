9d4952c75b9b1e8308af27cd37aa64c7d3f3367f
KotovSergeyV <75029707+KotovSergeyV@users.noreply.github.com>
2023-10-28 15:53:30 +0300

    Portals

--- src/Snake/Assets/NG-Portal/Portal.cs
+++ src/Snake/Assets/NG-Portal/Portal.cs
@@ -8,6 +8,9 @@ public class Portal : MonoBehaviour
     [SerializeField] private float _cooldownDuration;
     private static bool _canTeleport = true;
 
+    [SerializeField] private Sprite _sprite;
+    [SerializeField] private Sprite _altSprite;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (_canTeleport && collision.gameObject.CompareTag("Player"))
@@ -18,13 +21,17 @@ public class Portal : MonoBehaviour
 
     private IEnumerator TeleportPlayer(GameObject player)
     {
+        gameObject.GetComponent<SpriteRenderer>().sprite = _altSprite;
         _canTeleport = false;
         gameObject.layer = 11;
         _destinationPortal.gameObject.layer = 11;
+        _destinationPortal.gameObject.gameObject.GetComponent<SpriteRenderer>().sprite = _altSprite;
         player.transform.position = _destinationPortal.position + new Vector3(1f, 0f, 0f);
         yield return new WaitForSeconds(_cooldownDuration);
         _canTeleport = true;
         gameObject.layer = 0;
         _destinationPortal.gameObject.layer = 0;
+        gameObject.GetComponent<SpriteRenderer>().sprite = _sprite;
+        _destinationPortal.gameObject.gameObject.GetComponent<SpriteRenderer>().sprite = _sprite;
     }
 }
