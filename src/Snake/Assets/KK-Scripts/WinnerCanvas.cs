a0b6a8107549d7e55bcc28cdc69a8253f9f7405e
krl76 <k.kornilov2005@yandex.ru>
2023-10-29 11:36:08 +0300

    SoundOnWinner

--- src/Snake/Assets/KK-Scripts/WinnerCanvas.cs
+++ src/Snake/Assets/KK-Scripts/WinnerCanvas.cs
@@ -9,16 +9,24 @@ public class WinnerCanvas : MonoBehaviour
 {
     [SerializeField] Button _mainMenuButton;
     Action home;
+    AudioManager audioManager;
 
     private void Awake()
     {
+        audioManager = GetComponent<AudioManager>();
         home = toMenu;
         _mainMenuButton.onClick.RemoveAllListeners();
         _mainMenuButton.onClick.AddListener(home.Invoke);
     }
 
+    void InstantiateButtonSound()
+    {
+        Instantiate(Resources.Load("ButtonSoundPlayer"));
+    }
+
     private void toMenu()
     {
+        InstantiateButtonSound();
         SceneManager.LoadScene("MainMenu");
     }
 }
