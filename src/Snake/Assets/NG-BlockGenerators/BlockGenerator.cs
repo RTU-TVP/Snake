c2a319b5fb8313a8f5f7b4d4e51932990175a05b
El_Nico <g.nabiulin@yandex.ru>
2023-10-29 12:08:02 +0300

    Block-One

--- src/Snake/Assets/NG-BlockGenerators/BlockGenerator.cs
+++ src/Snake/Assets/NG-BlockGenerators/BlockGenerator.cs
@@ -24,9 +24,6 @@ public class BlockGenerator : MonoBehaviour
 
     private IEnumerator Start()
     {
-        SpawnObject();
-        SpawnObject();
-
         while (true)
         {
             SpawnObject();
